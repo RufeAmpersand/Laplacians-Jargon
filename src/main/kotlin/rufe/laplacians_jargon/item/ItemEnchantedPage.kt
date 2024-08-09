0b336772a52f2caf51dbc0c93897377c0efe30f0
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2024-08-09 23:17:21 +0900

    [feature/LJ-2] 二つのアイテムにレアリティを追加 多分要らない気もするけど

--- src/main/kotlin/rufe/laplacians_jargon/item/ItemEnchantedPage.kt
+++ src/main/kotlin/rufe/laplacians_jargon/item/ItemEnchantedPage.kt
@@ -1,8 +1,10 @@
 package rufe.laplacians_jargon.item
 
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.Rarity
 
 object ItemEnchantedPage:Item(
-    Properties().requiredFeatures()
+    Properties().requiredFeatures().rarity(Rarity.UNCOMMON)
 ) {
+    
 }
--- src/main/kotlin/rufe/laplacians_jargon/item/ItemPageOfWisdom.kt
+++ src/main/kotlin/rufe/laplacians_jargon/item/ItemPageOfWisdom.kt
@@ -1,8 +1,9 @@
 package rufe.laplacians_jargon.item
 
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.Rarity
 
 object ItemPageOfWisdom:Item(
-    Properties().requiredFeatures()
+    Properties().requiredFeatures().rarity(Rarity.COMMON)
 ) {
 }
