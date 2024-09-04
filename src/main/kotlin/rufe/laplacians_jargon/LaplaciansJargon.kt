1373ab0628fad25cba8f3c90fdde49e6cf384256
RufeAmpersand <97739788+RufeAmpersand@users.noreply.github.com>
2024-09-04 18:37:16 +0900

    [feature/LJ-31] Registry関連をKotlin For Forgeの機能を用いるように変更

--- src/main/kotlin/rufe/laplacians_jargon/LaplaciansJargon.kt
+++ src/main/kotlin/rufe/laplacians_jargon/LaplaciansJargon.kt
@@ -1,20 +1,16 @@
 package rufe.laplacians_jargon
 
-import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import rufe.laplacians_jargon.Laplacians_Jargon.BuildConfig
-import rufe.laplacians_jargon.creative_tab.LaplaciansJargonCreativeTab
+import rufe.laplacians_jargon.creative_tab.CreativeTabRegister
 import rufe.laplacians_jargon.register.ItemRegister
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(BuildConfig.MOD_ID)
-class LaplaciansJargon {
+object LaplaciansJargon {
     init{
-        val eventBus = FMLJavaModLoadingContext.get().modEventBus
 
-        ItemRegister.register(eventBus)
-        LaplaciansJargonCreativeTab.register(eventBus)
-
-        MinecraftForge.EVENT_BUS.register(this)
+        ItemRegister.register(MOD_BUS)
+        CreativeTabRegister.register(MOD_BUS)
     }
 }
